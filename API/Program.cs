abd1525b400895b3131b82ccc767c2c8571273ed
tobias esquivel <tobias-esquivel@hotmail.com>
2025-01-19 16:30:01 -0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -5,21 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-// Action<DbContextOptionsBuilder> accion = (DbContextOptionsBuilder opciones) =>{
-//     opciones.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-// };
-
-
 builder.Services.AddDbContext<DataContext>(
     (options) => {
         options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     }
 );
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
